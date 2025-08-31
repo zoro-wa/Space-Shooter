3090087d011f1d0700bb430f13453fd94bd11f3c
Anmol Raj Shakya <shakyaanmol33@gmail.com>
2025-08-31 17:49:02 +0545

    Used FRECT to place the images and created player animation

--- Space_Shooter/shooter.py
+++ Space_Shooter/shooter.py
@@ -1,6 +1,6 @@
 import pygame
-
 from os.path import join
+from random import randint
 
 
 #setup
@@ -17,7 +17,17 @@ x = 100
 
 #import an image
 player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha()
+player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2 , WINDOW_HEIGHT / 2))
+player_direction = 1
+
+meteor_surf = pygame.image.load(join('images', 'meteor.png'))
+meteor_rect = meteor_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+
+laser_surf = pygame.image.load(join('images', 'laser.png'))
+laser_rect = laser_surf.get_frect(bottomleft = (20, WINDOW_HEIGHT - 20))
+ 
 star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
+star_positions = [(randint(0,WINDOW_WIDTH),randint(0,WINDOW_HEIGHT)) for i in range(20)]
 
 while running:
     #event loop 
@@ -27,7 +37,17 @@ while running:
 
     #draw the game
     display_surf.fill('azure3')
-    x += 0.1
-    display_surf.blit(player_surf,(x,150))
+    for pos in star_positions:
+       display_surf.blit(star_surf,pos)
+
+    display_surf.blit(meteor_surf,meteor_rect)
+    display_surf.blit(laser_surf,laser_rect)
+    #player movement
+    player_rect.x += player_direction * 0.4
+    if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
+        player_direction *= -1
+    display_surf.blit(player_surf,player_rect)
+
     pygame.display.update()
+
 pygame.quit()
