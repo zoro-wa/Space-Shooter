1abc93759700eda730d07ecd53a69c3ba87b0314
Anmol Raj Shakya <shakyaanmol33@gmail.com>
2025-09-01 15:33:44 +0545

    Added Keyboard Inputs and adjusted player speed in all directions via vectors

--- Space_Shooter/shooter.py
+++ Space_Shooter/shooter.py
@@ -19,8 +19,8 @@ x = 100
 #import an image
 player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha()
 player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2 , WINDOW_HEIGHT / 2))
-player_direction = pygame.math.Vector2(2, -1)
-player_speed = 10
+player_direction = pygame.math.Vector2()
+player_speed = 300
 
 star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
 star_positions = [(randint(0,WINDOW_WIDTH),randint(0,WINDOW_HEIGHT)) for i in range(20)]
@@ -33,11 +33,19 @@ laser_surf = pygame.image.load(join('images', 'laser.png'))
 laser_rect = laser_surf.get_frect(bottomleft = (20, WINDOW_HEIGHT - 20))
 
 while running:
-    clock.tick(10)
+    dt = clock.tick() / 1000
     #event loop 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+         
+    #input
+    keys = pygame.key.get_pressed()
+    player_direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+    player_direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+    player_direction = player_direction.normalize() if player_direction else player_direction
+    player_rect.center += player_direction * player_speed * dt
 
     #draw the game
     display_surf.fill('azure3')
@@ -46,9 +54,7 @@ while running:
 
     display_surf.blit(meteor_surf,meteor_rect)
     display_surf.blit(laser_surf,laser_rect)
-    #player movement
-    player_rect.center += player_direction * player_speed
-    display_surf.blit(player_surf,player_rect.topleft)
+    display_surf.blit(player_surf,player_rect)
 
     pygame.display.update()
 
