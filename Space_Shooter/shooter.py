b3b631ece0ff8f557190f8b4581c15a8177269d2
Anmol Raj Shakya <shakyaanmol33@gmail.com>
2025-09-02 18:02:09 +0545

    Now the meteors will rotate

--- Space_Shooter/shooter.py
+++ Space_Shooter/shooter.py
@@ -14,11 +14,10 @@ class Player(pygame.sprite.Sprite):
         self.can_shoot = True
         self.laser_shoot_time = 0
         self.cooldown_duration = 400
-        
+
         #mask
         self.mask = pygame.mask.from_surface(self.image)
         
-
     def laser_timer(self):
         if not self.can_shoot:
             current_time = pygame.time.get_ticks()   
@@ -40,6 +39,7 @@ class Player(pygame.sprite.Sprite):
 
         self.laser_timer()
 
+      
 class Star(pygame.sprite.Sprite):
     def __init__(self, groups, surf):
         super().__init__(groups)
@@ -60,18 +60,25 @@ class Laser(pygame.sprite.Sprite):
 class Meteor(pygame.sprite.Sprite):
     def __init__(self, surf, pos, groups):
         super().__init__(groups)
+        self.original_surf = surf
         self.image = surf
         self.rect = self.image.get_frect(center = pos)
         self.start_time = pygame.time.get_ticks()
         self.lifetime = 3000
         self.direction = pygame.Vector2(uniform(-0.5, 0.5), 1)
         self.speed = randint(400,500)
+        self.rotation_speed = randint(40, 80)
+        self.rotation = 0
        
     def update(self, dt):
         self.rect.center += self.direction * self.speed * dt
         if pygame.time.get_ticks() - self.start_time >= self.lifetime:
             self.kill()
-        
+         
+        self.rotation += self.rotation_speed * dt
+        self.image = pygame.transform.rotozoom(self.original_surf, self.rotation, 1)
+        self.rect = self.image.get_frect(center = self.rect.center)
+
 def collisions():
     global running
 
